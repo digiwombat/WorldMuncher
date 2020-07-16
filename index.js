b05be157bfb66396948c01cf2c3fbc44b5445f79
Randall Fitzgerald <randall@dasaku.net>
2020-07-16 00:09:20 -0700

    More prep-work on dynamic layouts. Updates to basically all the project dependencies.
    Updates improve spellcheck coverage. Everything should work if you want to just pull and npm start. Not doing a version bump or release until dynamic layouts are done.
    Creating layouts seems fine for now, but all the backend handling isn't done yet.

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const {app, BrowserWindow} = require('electron')
 const windowStateKeeper = require('electron-window-state');
+const gotTheLock = app.requestSingleInstanceLock();
+
+let win;
 
 
-let win
-
-
-function createWindow () {
+function createWindow() 
+{
 	let mainWindowState = windowStateKeeper({
 		defaultWidth: 1280,
 		defaultHeight: 800
@@ -17,8 +18,50 @@ function createWindow () {
 		'y': mainWindowState.y,
 		'width': mainWindowState.width,
 		'height': mainWindowState.height,
-		frame: false
+		frame: false,
+		webPreferences: {
+			spellcheck: true,
+			nodeIntegration: true,
+			enableRemoteModule: true
+		}
 	});
+
+	const {
+		Menu,
+		MenuItem
+	} = require('electron')
+
+	win.webContents.on('context-menu', (event, params) => {
+		const menu = new Menu();
+		if (!event.target.closest('textarea, input, [contenteditable="true"]')) return;
+		// Add each spelling suggestion
+		//console.log(params);
+		for (const suggestion of params.dictionarySuggestions)
+		{
+			menu.append(new MenuItem({
+				label: suggestion,
+				click: () => mainWindow.webContents.replaceMisspelling(suggestion)
+			}))
+		}
+
+		// Allow users to add the misspelled word to the dictionary
+		if (params.misspelledWord)
+		{
+			menu.append(
+				new MenuItem({
+					type: 'separator'
+				})
+			)
+			menu.append(
+				new MenuItem({
+					label: 'Add to dictionary',
+					click: () => win.webContents.session.addWordToSpellCheckerDictionary(params.misspelledWord)
+				})
+			)
+		}
+
+		menu.popup()
+	})
 
 	mainWindowState.manage(win);
 
@@ -33,28 +76,42 @@ function createWindow () {
 }
 
 
-const shouldQuit = app.makeSingleInstance((commandLine, workingDirectory) => {
-	if (win)
-	{
-		if (win.isMinimized()) win.restore()
-		win.webContents.send('new', commandLine);
-		win.focus()
-	}
-})
 
-if (shouldQuit) {
+
+
+if (!gotTheLock) {
 	app.quit()
 }
+else
+{
+	app.on('second-instance', (event, commandLine, workingDirectory) => {
+		// Someone tried to run a second instance, we should focus our window.
+		if (myWindow)
+		{
+			if (myWindow.isMinimized())
+			{
+				myWindow.restore()
+			}
+			myWindow.focus()
+		}
+	})
 
-app.on('ready', createWindow)
+	// Create myWindow, load the rest of the app, etc...
+	app.whenReady().then(() => {
+		createWindow();
+	})
+}
 
-app.on('window-all-closed', () => {
+app.on('window-all-closed', () =>
+{
 	app.quit()
-})
+});
 
-app.on('activate', () => {
-	if (win === null) {
-	createWindow()
+app.on('activate', () =>
+{
+	if (win === null) 
+	{
+		createWindow();
 	}
 
-})
+});
